a8955817e46fde268b47f66c3d2ad7f3c389dbde
JIwan <sharmaj@wsc.school.nz>
2026-06-15 11:59:38 +1200

    Completed lesson 9

--- Lesson 1 - Output and input/_main.py
+++ Lesson 1 - Output and input/_main.py
@@ -1,3 +1,6 @@
-import random
-shopping_list = ['apples','plums','pizza']
-print(shopping_list[random.randint(0,2)])
+user_input = 'idk'
+try:
+    num = int(user_input)
+    print(f'You picked {num}')
+except:
+	print(f'{user_input} is not a number!')
