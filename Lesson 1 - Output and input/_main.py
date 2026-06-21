c3d26e22d94c8a6441e0947dcbfd6d3bd1002d9a
JIwan <sharmaj@wsc.school.nz>
2026-06-22 11:57:34 +1200

    wrote and understood what return does.

--- Lesson 1 - Output and input/_main.py
+++ Lesson 1 - Output and input/_main.py
@@ -1,6 +1,12 @@
-user_input = 'idk'
-try:
-    num = int(user_input)
-    print(f'You picked {num}')
-except:
-	print(f'{user_input} is not a number!')
+def get_number():
+    while True:
+        num = input("Give me a number.")
+        try:
+            num = int(num)
+            return
+        except:
+            print("That's not a number")
+
+num = get_number()
+
+print(num)
