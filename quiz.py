aea0f6883b973b02b252322b23575a0e9d566502
JIwan <sharmaj@wsc.school.nz>
2026-06-23 15:07:47 +1200

    made a decent quiz that works.
    needs more questions, score and just make it better.

---
+++ quiz.py
@@ -0,0 +1,27 @@
+question_1 = input("What planet is known as the red planet?") .lower() .strip()
+if question_1 == ("mars"):
+    print("correct.")
+else:
+    print("incorrect.")
+# If they answer with either "pacific ocean" or "pacific" it will be correct, otherwise it will be incorrect.
+question_2 = ["pacific ocean", "pacific"]
+question_2 = input("what is the largest ocean in the world?") .lower() .strip()
+if question_2 in question_2:
+    print("correct.")
+else:
+    print("incorrect.")
+question_3 = input("What is the capital of United Kingdom?") .lower() .strip()
+if question_3 == ("london"):
+    print("correct.")
+else:
+    print("incorrect.")
+question_4 = input("What is the largest mammal?") .lower() .strip()
+if question_4 == ("blue whale"):
+    print("correct.")
+else:
+    print("incorrect.")
+question_5 = input("What is the chemical symbol for gold?") .lower() .strip()
+if question_5 == ("au"):
+    print("correct.")
+else:
+    print("incorrect.")
